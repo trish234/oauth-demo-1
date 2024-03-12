8ebcb87250b70bf303a353fbb4ef31289fc3f271
Trisha Cox <15840948+trish234@users.noreply.github.com>
2024-03-11 21:30:05 -0700

    make node project

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express')
+const app = express()
+const port = 3001
+
+// View Engine Setup 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
