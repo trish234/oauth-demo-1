f2b1ef0c83dd35b8fdc5b615bbc1943f85b6640b
Trisha Cox <15840948+trish234@users.noreply.github.com>
2024-03-30 19:29:11 -0700

    happy path

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-// View Engine Setup 
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.get('/redirect', (req, res) => {
   res.sendFile(path.join(__dirname, 'redirect.html'));
+})
+app.get('/display-resource', (req, res) => {
+  res.sendFile(path.join(__dirname, 'displayResource.html'));
 })
 
 app.listen(port, () => {
